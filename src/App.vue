a63549ee5dea965d5f18746e2b66e687fd09bc64
wanjie <wanjie@oushu.io>
2021-11-08 17:00:58 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <x-input
+    class="raw"
+    :rules="[{ required: true, message: '请输入主计算集群名称' }]"
+    v-model:value="test"
+  ></x-input>
+  <x-dropdown>
+    <a class="ant-dropdown-link" @click="e => e.preventDefault()">
+      Hover me
+    </a>
+    <template #overlay>
+      <x-menu>
+        <x-menu-item>
+          <a href="javascript:;">1st menu item</a>
+        </x-menu-item>
+        <x-menu-item>
+          <a href="javascript:;">2nd menu item</a>
+        </x-menu-item>
+        <x-menu-item>
+          <a href="javascript:;">3rd menu item</a>
+        </x-menu-item>
+      </x-menu>
+    </template>
+  </x-dropdown>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+import XInput from './SmartUI/XInput.vue'
+import XDropdown from './SmartUI/XDropdown.vue'
+import XMenu from './SmartUI/XMenu.vue'
+import XMenuItem from './SmartUI/XMenuItem.vue'
+export default {
+  components: {
+    XInput,
+    XDropdown,
+    XMenu,
+    XMenuItem,
+  },
+  setup() {
+    return {
+      test: ref('')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  padding: 100px 20px;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [
+        AntDesignVueResolver()
+      ]
+    })
+  ],
+  server: {
+    port: 4000
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'SmartUi',
+      fileName: (format) => `smart-ui.${format}.js`
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
